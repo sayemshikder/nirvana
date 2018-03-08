9c39cc17d5f87b40e4058f2fb7b8b8810963cc0c
Marvin Yan <marvin.yan@outlook.com>
2018-03-08 18:45:17 -0500

    Update font-face CSS to use font-url and remove spaces from font file names

--- config/application.rb
+++ config/application.rb
@@ -15,5 +15,6 @@ module Nirvana
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
 
+    config.assets.paths << Rails.root.join("app", "assets", "fonts")
   end
 end
